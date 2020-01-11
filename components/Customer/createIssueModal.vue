eb8ecef960381637cdc44503d1c945580bcb442f
Abdellah Chadidi <chadidi97@gmail.com>
2020-01-11 19:47:39 +0100

    Create issue model component

---
+++ components/Customer/createIssueModal.vue
@@ -0,0 +1,66 @@
+<template>
+  <v-dialog :value="visibility" persistent max-width="600px">
+    <v-card class="pa-6">
+      <div class="title blue--text mb-4">create new issue</div>
+      <v-form>
+        <v-text-field v-model="issue.title" dense outlined label="title" />
+        <v-textarea v-model="issue.description" outlined label="description" />
+        <v-combobox
+          v-model="issue.tags"
+          outlined
+          multiple
+          label="tags"
+          append-icon
+          chips
+          deletable-chips
+        />
+        <div class="d-flex justify-space-between">
+          <v-btn depressed @click="cancel">
+            cancel
+          </v-btn>
+          <v-btn depressed @click="create" class="white--text" color="green">
+            create
+          </v-btn>
+        </div>
+      </v-form>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+import { mapState } from "vuex";
+
+export default {
+  data() {
+    return {
+      issue: {
+        title: "",
+        description: "",
+        tags: []
+      }
+    };
+  },
+  computed: {
+    ...mapState({
+      visibility: state => state.Customer.Issues.createModalVisibility
+    })
+  },
+  methods: {
+    cancel() {
+      this.$store.commit("Customer/Issues/SET_CREATE_MODAL_VISIBILITY", false);
+      this.issue.title = "";
+      this.issue.description = "";
+      this.issue.tags = [];
+    },
+    create() {
+      this.$store.commit("Customer/Issues/UNSHIFT", {
+        ...this.issue,
+        status: "SUBMITTED"
+      });
+      this.cancel();
+    }
+  }
+};
+</script>
+
+<style></style>
