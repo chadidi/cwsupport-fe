2605fad81564c8798cf7d3e12ef1f7ac954b9237
Abdellah Chadidi <chadidi97@gmail.com>
2020-01-12 16:50:59 +0100

    Update customer page

--- pages/customer.vue
+++ pages/customer.vue
@@ -5,6 +5,16 @@
       <v-spacer></v-spacer>
       <v-toolbar-items>
         <v-btn
+          to="/admin"
+          depressed
+          class="white--text"
+          color="green"
+          v-if="$store.state.auth.user.is_admin"
+        >
+          <v-icon left>mdi-account-convert</v-icon>
+          Admin Dash
+        </v-btn>
+        <v-btn
           @click="openCreateModal"
           depressed
           class="white--text"
@@ -13,8 +23,13 @@
           <v-icon left>mdi-plus-circle</v-icon>
           create issue
         </v-btn>
-        <v-btn @click.prevent="logout" class="white--text" color="red">
-          <v-icon left>mdi-location-exit</v-icon>
+        <v-btn
+          @click.prevent="logout"
+          depressed
+          class="white--text"
+          color="red"
+        >
+          <v-icon>mdi-location-exit</v-icon>
         </v-btn>
       </v-toolbar-items>
     </v-toolbar>
@@ -30,7 +45,7 @@ import createIssueModal from "~/components/Customer/createIssueModal";
 import logout from "@/graphql/logout.gql";
 
 export default {
-  middleware: "isAuth",
+  middleware: ["isAuth"],
   components: {
     createIssueModal
   },
