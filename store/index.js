afbb496141672fbe849f932c8bb1c3cacc9126b6
Abdellah Chadidi <chadidi97@gmail.com>
2020-01-12 16:16:24 +0100

    Create store index and dispatch auth user fetch on first load

---
+++ store/index.js
@@ -0,0 +1,39 @@
+export const state = () => ({
+  status: {
+    SUBMITTED: {
+      name: "Submitted",
+      color: "blue",
+      icon: "mdi-checkbox-marked-circle"
+    },
+    IN_PROGRESS: {
+      name: "In Progress",
+      color: "orange",
+      icon: "mdi-clock"
+    },
+    RESOLVED: {
+      name: "Resolved",
+      color: "green",
+      icon: "mdi-checkbox-marked-circle"
+    },
+    CLOSED: {
+      name: "Closed",
+      color: "red",
+      icon: "mdi-lock"
+    }
+  }
+})
+
+export const mutations = {
+
+}
+
+export const actions = {
+  async nuxtServerInit({
+    dispatch
+  }, {
+    store,
+    app
+  }) {
+    await dispatch('auth/fetchUser')
+  }
+}
